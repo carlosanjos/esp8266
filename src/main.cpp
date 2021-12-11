20ca340aaa76c42462423359fadfeb128b537f0b
Carlos Anjos <anjos.carlos@gmail.com>
2021-12-11 17:56:30 +0000

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+#define LOGO_HEIGHT 16
+#define LOGO_WIDTH 16
+
+void setup()
+{
+    Serial.begin(9600);
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    { // Address 0x3D for 128x64
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ; // Don't proceed, loop forever
+    }
+}
+
+void testscrolltext(void)
+{
+    display.clearDisplay();
+
+    display.setTextSize(2); // Draw 2X-scale text
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(10, 0);
+    display.println(F("Mozones"));
+    display.display(); // Show initial text
+    delay(100);
+
+    // Scroll in various directions, pausing in-between:
+    display.startscrollright(0x00, 0x0F);
+    delay(2000);
+    display.stopscroll();
+    delay(1000);
+    display.startscrollleft(0x00, 0x0F);
+    delay(2000);
+    display.stopscroll();
+    delay(1000);
+    display.startscrolldiagright(0x00, 0x07);
+    delay(2000);
+    display.startscrolldiagleft(0x00, 0x07);
+    delay(2000);
+    display.stopscroll();
+    delay(1000);
+}
+
+
+void loop()
+{
+    testscrolltext(); // Draw scrolling text
+    //  testdrawchar();
+}
+
+void testdrawchar(void)
+{
+    display.clearDisplay();
+
+    display.setTextSize(1.5);            // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.setCursor(0, 0);             // Start at top-left corner
+    display.cp437(true);                 // Use full 256 char 'Code Page 437' font
+
+    // Not all the characters will fit on the display. This is normal.
+    // Library will draw what it can and the rest will be clipped.
+    for (int16_t i = 0; i < 256; i++)
+    {
+        if (i == '\n')
+            display.write(' ');
+        else
+            display.write(i);
+    }
+
+    display.display();
+    delay(2000);
+}
